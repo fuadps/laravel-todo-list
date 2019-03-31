7aa30cf8383f5c501e3fc92dc440f34ce65ab1bc
Fuad <fuadpls@gmail.com>
2019-03-31 17:03:40 +0800

    Create - Index Views and Traits

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Traits\TimestampsTrait;
 
 class Task extends Model
 {
-    //
+    use TimestampsTrait;
+    
+    // define the table
+    protected $table = 'tasks';
 }
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.main')
+
+@section('title','Task Home')
+
+@section('content')
+
+    @foreach ($tasks as $task)
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>
+                    {{ $task->name }}
+                    <small> {{$task->created_at}} </small>
+                </h3>
+                <p> {{$task->description}} </p>
+                <h4> {{$task->due_date}} </h4>
+            </div>
+        </div>
+        <hr>
+    @endforeach
+
+    <div class="row justify-content-center">
+        <div class="col-sm-6 text-center">
+            {{ $tasks->links() }}
+        </div>
+    </div>
+    
+@endsection
